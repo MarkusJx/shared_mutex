7b8b998b51aab4483f3975a80e7a4b577db6e015
Markus <28785953+markusjx@users.noreply.github.com>
2021-02-02 21:46:35 +0100

    Add comments
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- addon.cpp
+++ addon.cpp
@@ -6,15 +6,21 @@
 
 #define export(func) exports.Set(std::string("lib_") + #func, Napi::Function::New(env, func))
 
+// The mutexes map pointer storing all created mutexes and their names
 std::unique_ptr<std::map<std::string, shared_mutex>> mutexes = nullptr;
 
+/**
+ * Create a shared_mutex
+ */
 void createMutex(const Napi::CallbackInfo &info) {
+    // Check if info has a length of 1 and info[0] is a string
     if (info.Length() != 1) {
         throw Napi::Error::New(info.Env(), "Expected one argument");
     } else if (!info[0].IsString()) {
         throw Napi::Error::New(info.Env(), "Expected argument type string at position 0");
     }
 
+    // Try creating the mutex, rethrowing all occurring errors
     try {
         mutexes->insert(
                 std::pair<std::string, shared_mutex>(info[0].ToString(), std::move(shared_mutex(info[0].ToString()))));
@@ -24,14 +30,20 @@ void createMutex(const Napi::CallbackInfo &info) {
 }
 
 void deleteMutex(const Napi::CallbackInfo &info) {
+    // Check if info has a length of 1 and info[0] is a string
     if (info.Length() != 1) {
         throw Napi::Error::New(info.Env(), "Expected one argument");
     } else if (!info[0].IsString()) {
         throw Napi::Error::New(info.Env(), "Expected argument type string at position 0");
     }
 
+    // Try erasing the mutex, rethrowing all occurring errors
     try {
-        mutexes->erase(info[0].ToString().Utf8Value());
+        if (mutexes->find(info[0].ToString().Utf8Value()) != mutexes->end()) {
+            mutexes->erase(info[0].ToString().Utf8Value());
+        } else {
+            throw std::runtime_error("The mutex with the given name does not exist");
+        }
     } catch (const std::exception &e) {
         throw Napi::Error::New(info.Env(), e.what());
     }
@@ -41,9 +53,11 @@ void deleteMutex(const Napi::CallbackInfo &info) {
  * Export all functions
  */
 Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+    // Export the functions
     export(createMutex);
     export(deleteMutex);
 
+    // Try creating the mutexes map pointer
     try {
         mutexes = std::make_unique<std::map<std::string, shared_mutex>>();
     } catch (...) {
--- shared_mutex.hpp
+++ shared_mutex.hpp
@@ -43,67 +43,125 @@ private:
     const std::string error;
 };
 
+/**
+ * A shared mutex
+ */
 class shared_mutex {
 public:
-    explicit shared_mutex(std::string mtx_name) : prog_name(std::move(mtx_name)) {
-        if (std::find(owned_mutexes.begin(), owned_mutexes.end(), prog_name) != owned_mutexes.end()) {
-            throw shared_mutex_exception("A mutex with the name '" + prog_name + "' is already owned by this program");
+    /**
+     * Create a shared_mutex instance.
+     * Throws an exception if another shared_mutex with the same name already exists.
+     *
+     * @param mutex_name the mutex name
+     */
+    explicit shared_mutex(std::string mutex_name) : mtx_name(std::move(mutex_name)) {
+        // If mtx_name already exists in owned_mutexes, throw an exception
+        if (std::find(owned_mutexes.begin(), owned_mutexes.end(), mtx_name) != owned_mutexes.end()) {
+            throw shared_mutex_exception("A mutex with the name '" + mtx_name + "' is already owned by this program");
         }
 
 #ifdef OS_WINDOWS
+        // Create the name for the mutex
         std::string name = "Local\\";
-        name.append(prog_name);
+        name.append(mtx_name);
 
+        // Try to create the mutex
         HANDLE mutex = CreateMutex(nullptr, false, name.c_str());
 
+        // If the mutex already exists, throw an exception
         if (GetLastError() == ERROR_ALREADY_EXISTS) {
             throw shared_mutex_exception(
-                    "A mutex with the name '" + prog_name + "' is already owned by another program");
+                    "A mutex with the name '" + mtx_name + "' is already owned by another program");
         } else {
+            // Set the handle to mutex and add the mutex name to owned_mutexes
             this->handle = mutex;
-            owned_mutexes.push_back(prog_name);
+            owned_mutexes.push_back(mtx_name);
         }
 #else
+        // Create the name for the mutex
         std::string name = "/";
-        name.append(prog_name);
+        name.append(mtx_name);
 
+        // Try to create the semaphore
         sem_t *semaphore = sem_open(name.c_str(), O_CREAT | O_EXCL, PERM, 0);
+
+        // If the creation failed, thrown an exception
         if (semaphore == SEM_FAILED) {
-            throw shared_mutex_exception("A mutex with the name '" + prog_name + "' is already owned by another program");
+            throw shared_mutex_exception("A mutex with the name '" + mtx_name + "' is already owned by another program");
         } else {
+            // Set the semaphore and add the mutex name to owned_mutexes
             this->semaphore = semaphore;
-            owned_mutexes.push_back(prog_name);
+            owned_mutexes.push_back(mtx_name);
         }
 #endif //OS_WINDOWS
     }
 
+    /**
+     * No copy constructor
+     */
+    shared_mutex(const shared_mutex &) = delete;
+
+    /**
+     * Move constructor
+     *
+     * @param rhs the shared_mutex to move
+     */
+    shared_mutex(shared_mutex &&rhs) noexcept: mtx_name(std::move(rhs.mtx_name)) {
+        // Set the handle or semaphore of rhs to nullptr
 #ifdef OS_WINDOWS
-
-    shared_mutex(shared_mutex &&rhs) noexcept: handle(rhs.handle), prog_name(std::move(rhs.prog_name)) {
+        this->handle = rhs.handle;
         rhs.handle = nullptr;
-    }
-
 #else
-
-    shared_mutex(shared_mutex &&rhs) noexcept: semaphore(rhs.semaphore), prog_name(std::move(rhs.prog_name)) {
+        this->semaphore = rhs.semaphore;
         rhs.semaphore = nullptr;
+#endif //OS_WINDOWS
     }
 
+    /**
+     * No copy assignment operator
+     */
+    shared_mutex &operator=(const shared_mutex &) = delete;
+
+    /**
+     * Move assignment operator
+     *
+     * @param rhs the instance to assign
+     * @return this
+     */
+    shared_mutex &operator=(shared_mutex &&rhs) noexcept {
+        // Swap all class members
+        std::swap(rhs.mtx_name, this->mtx_name);
+#ifdef OS_WINDOWS
+        std::swap(rhs.handle, this->handle);
+#else
+        std::swap(rhs.semaphore, this->semaphore);
 #endif //OS_WINDOWS
 
+        return *this;
+    }
+
+    /**
+     * Delete this shared_mutex.
+     * Destroys the semaphore or mutex.
+     */
     ~shared_mutex() {
 #ifdef OS_WINDOWS
+        // If the handle is null, return
         if (handle == nullptr) return;
 
+        // Close the handle to the mutex
         if (!CloseHandle(handle)) {
             std::cerr << "Could not close the handle to the mutex" << std::endl;
         }
 #else
+        // If the semaphore is null, return
         if (semaphore == nullptr) return;
 
+        // Re-create the semaphore name
         std::string name = "/";
-        name.append(prog_name);
+        name.append(mtx_name);
 
+        // Close and delete the semaphore
         if (sem_close(semaphore) == -1) {
             std::cerr << "Could not close the semaphore" << std::endl;
         } else if (sem_unlink(name.c_str()) == -1) {
@@ -111,9 +169,10 @@ public:
         }
 #endif //OS_WINDOWS
 
+        // Try to delete the mutex name from the owned_mutexes vector, if it exists in it
         try {
-            if (std::find(owned_mutexes.begin(), owned_mutexes.end(), prog_name) != owned_mutexes.end()) {
-                owned_mutexes.erase(std::find(owned_mutexes.begin(), owned_mutexes.end(), prog_name));
+            if (std::find(owned_mutexes.begin(), owned_mutexes.end(), mtx_name) != owned_mutexes.end()) {
+                owned_mutexes.erase(std::find(owned_mutexes.begin(), owned_mutexes.end(), mtx_name));
             }
         } catch (...) {}
     }
@@ -121,10 +180,13 @@ public:
 private:
     static inline std::vector<std::string> owned_mutexes = std::vector<std::string>();
 
-    std::string prog_name;
+    // The mutex name
+    std::string mtx_name;
 #ifdef OS_WINDOWS
+    // The handle to the mutex
     HANDLE handle;
 #else
+    // A pointer to the semaphore
     sem_t *semaphore;
 #endif //OS_WINDOWS
 };
